2a957e9c2a3aa4a8247555c610221655e612377d
acdlc4 <adriandelacruz@computer-one.attlocal.net>
2024-07-21 16:56:19 -0500

    updates to SQL schema and ER diagram

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,4 +1,14 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿--Create a database named '''crowdfunding_db''' using SQL code
+
+CREATE DATABASE crowdfunding_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Modify this code to update the DB schema diagram.
@@ -9,7 +19,7 @@ CREATE TABLE "category" (
     "category_id" varchar(4)   NOT NULL,
     "category" varchar(12)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id","category"
+        "category_id"
      )
 );
 
@@ -17,27 +27,7 @@ CREATE TABLE "subcategory" (
     "subcategory_id" varchar(8)   NOT NULL,
     "subcategory" varchar(17)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id","subcategory"
-     )
-);
-
-CREATE TABLE "campaign" (
-    "cf_id" int64   NOT NULL,
-    "contact_id" int64   NOT NULL,
-    "company_name" varchar   NOT NULL,
-    "description" varchar   NOT NULL,
-    "goal" float   NOT NULL,
-    "pledged" float   NOT NULL,
-    "outcome" varchar(10)   NOT NULL,
-    "backers_count" int64   NOT NULL,
-    "country" varchar(2)   NOT NULL,
-    "currency" varchar(3)   NOT NULL,
-    "launched_date" datetime   NOT NULL,
-    "end_date" datetime   NOT NULL,
-    "category_id" varchar(4)   NOT NULL,
-    "subcategory_id" varchar(12)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id","contact_id"
+        "subcategory_id"
      )
 );
 
@@ -50,6 +40,26 @@ CREATE TABLE "contacts" (
         "contact_id"
      )
 );
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(10)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(2)   NOT NULL,
+    "currency" varchar(3)   NOT NULL,
+    "launched_date" timestamp   NOT NULL,
+    "end_date" timestamp   NOT NULL,
+    "category_id" varchar(4)   NOT NULL,
+    "subcategory_id" varchar(12)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id","contact_id"
+     )
+);
+
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
